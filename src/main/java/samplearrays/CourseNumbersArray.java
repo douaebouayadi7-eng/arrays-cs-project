2465f673445565d5b06c2cbbbd00e9390530ae44
douae <douaebouayadi7@gmail.com>
2026-09-23 22:09:39 +0000

    Update : CourseNumbersArrays

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -17,12 +17,18 @@ public class CourseNumbersArray {
         System.out.println();
     }
     public boolean checkCourses(int number){
-        for(course: registeredCourses){
+        for(int course: registeredCourses){
             if (course== number) return true;
         }
         return false;
     }
     public static void main(String[] args) {
+        CourseNumbersArray student= new CourseNumbersArray();
+        System.out.println("Initial registered courses: ");
+        student.printCourses();
+        student.addCourse(6767);
+        System.out.println("\nCourses after adding course 6767");
+        student.printCourses();
 
     }
 }
