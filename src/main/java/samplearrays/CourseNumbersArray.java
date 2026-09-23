989f5e6957d2d24bb1747c2d4557de2908c33946
douae <douaebouayadi7@gmail.com>
2026-09-23 20:49:20 +0000

    addCourse method is defined, is copies the registered courses and adds the new one.

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -2,7 +2,7 @@ package samplearrays;
 
 public class CourseNumbersArray {
     private int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
-    public addCourse(int newCourse){
+    public addCourses(int newCourse){
         int[] updatedCourses= new int[registeredCourses.length+ 1];
         for (int i=0; i<registeredCourses.length; i++){
             updatedCourses[i]= registeredCourses[i];
