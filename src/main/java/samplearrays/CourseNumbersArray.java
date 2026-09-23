dcb247c167366a37e0217d97cf7874bf0e9efe85
douae <douaebouayadi7@gmail.com>
2026-09-23 21:10:24 +0000

    printCourses is defined, it prints all of the registeredCourses.

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -2,13 +2,19 @@ package samplearrays;
 
 public class CourseNumbersArray {
     private int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
-    public addCourses(int newCourse){
+    public void addCourse(int newCourse){
         int[] updatedCourses= new int[registeredCourses.length+ 1];
         for (int i=0; i<registeredCourses.length; i++){
             updatedCourses[i]= registeredCourses[i];
         }
         updatedCourses[registeredCourses.length]= newCourse;
         registeredCourses=updatedCourses;
+    }
+    public void printCourses(){
+        for (int course: registeredCourses){
+            System.out.println(course+" ");
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
 
